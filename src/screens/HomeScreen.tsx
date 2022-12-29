2b6517831ab2179d03ce1fd3450c00ba75215fa5
LUCIA <luci3220@hotmail.com>
2022-12-28 21:13:26 -0300

    initial navigation setup

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button, Text, View } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+  <View>
+    <Text> HOME </Text>
+    
+  </View>
+  )
+}
+
+export default HomeScreen;
