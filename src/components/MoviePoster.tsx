f2320a00ca5b1f596810f0f596c65aca3dba6ad7
LUCIA <luci3220@hotmail.com>
2023-02-13 19:21:43 -0300

    add route -detail-

--- src/components/MoviePoster.tsx
+++ src/components/MoviePoster.tsx
@@ -1,5 +1,7 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react'
 import { View, Text, Image,  } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Movie } from '../interface/movieInterface';
 import './MoviePosterThemes'
 import { styles } from './MoviePosterThemes';
@@ -14,16 +16,20 @@ export const MoviePoster = ({ movie, height=420, width=300 }: PropsMovie) => {
  
 
 const uri= `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+const navigator = useNavigation();
 
 return (
-    <View style={{width, height, marginHorizontal:7}}>
+    <TouchableOpacity  
+    activeOpacity={0.9}
+    onPress={()=> navigator.navigate('DetailScreen')}
+      style={{width, height, marginHorizontal:7}}>
       <View style={styles.image_container}>
         <Image
         source={{uri}}
         style={styles.image}
        />
        </View>
-    </View>
+    </TouchableOpacity >
   )
 }
 
