08387678bcf87965f15053731786851428fb5ea8
LUCIA <luci3220@hotmail.com>
2023-02-27 15:16:45 -0300

    ScreenDetail Style

--- src/components/MoviePosterThemes.tsx
+++ src/components/MoviePosterThemes.tsx
@@ -1,29 +1,40 @@
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 
-export const styles= StyleSheet.create({
-    image:{
-       flex:1,
-       borderRadius:18,
-
+export const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    borderRadius: 18,
+  },
+  view: {
+    width: 300,
+    height: 420,
+  },
+  image_container: {
+    flex: 1,
+    borderRadius: 18,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    view:{
-        width:300,
-        height:420,
-        
-    },
-    image_container:{
-        flex:1,
-        borderRadius:18,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 6,
-        
-        elevation: 10,
+    shadowOpacity: 0.25,
+    shadowRadius: 6,
 
-    }
-})
-
+    elevation: 10,
+  },
+  imagen_poster: {
+    flex: 1,
+  },
+  margin_container: {
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  subTitle: {
+    fontSize: 16,
+    opacity: 0.8,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,7 +1,11 @@
 import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react'
-import { Text, View } from 'react-native';
+import { Image, Text, View, Dimensions, StyleSheet } from 'react-native';
 import { RootStackParam } from '../navigation/NavigationControllers';
+import { styles } from '../components/MoviePosterThemes';
+import { ScrollView } from 'react-native-gesture-handler';
+
+const screenHeigth= Dimensions.get('screen').height
 
 interface PropsDetailScreen extends StackScreenProps<RootStackParam, 'DetailScreen'> {};
 
@@ -9,12 +13,45 @@ interface PropsDetailScreen extends StackScreenProps<RootStackParam, 'DetailScre
 const DetailScreen = ({route}: PropsDetailScreen) => {
 
   const movie=route.params
+  const uri= `https://image.tmdb.org/t/p/w500${movie.poster_path}`
   
   return (
-  <View>
-    <Text> DETAIL</Text>
+    <ScrollView>
+      
+  <View style={style.imagen_container}>
+    <Image
+    source={{uri}}
+    
+    style={styles.imagen_poster}
+    />
+    
   </View>
+  <View style={styles.margin_container}>
+    <Text style={styles.subTitle}>{movie.original_title}</Text>
+    <Text style={styles.title}>{movie.title}</Text>
+  </View>
+  </ScrollView>
   )
 }
+
+const style= StyleSheet.create({
+  imagen_container:{
+    width:'100%',
+    height:screenHeigth * 0.7,
+    // backgroundColor:'red',
+
+    paddingBottom:15,
+    shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 6,
+        
+        elevation: 3,
+        borderBottomEndRadius:15
+}
+})
 
 export default DetailScreen;                                              
