e8e618654dfd267431237ae068b3e1a16eaa19e4
LUCIA <luci3220@hotmail.com>
2023-03-06 22:04:50 -0300

    fix Hook

--- src/hooks/useMovieDetail.tsx
+++ src/hooks/useMovieDetail.tsx
@@ -3,21 +3,37 @@ import { View } from 'react-native';
 import { useState, useEffect } from 'react';
 import moviesDB from '../api/moviesDb';
 import { MovieFull } from '../interface/movieInterface';
+import { CreditsMovie, Cast } from '../interface/creditsInterface';
 
 
 interface MovieDetailProps{
     isLoading:boolean;
-    movieFull:MovieFull
-    cost:any[]
+    movieFull?:MovieFull
+    cast:Cast[];
 }
+
+//INITIAL STATE
 const useMovieDetail = (movieId:number) => {
- const[state, setState]=useState<MovieDetailProps>();
+ const[state, setState]=useState<MovieDetailProps>({
+    isLoading:true,
+    movieFull:undefined,
+    cast:[],
+ });
   
 
  const getMovieDetails = async()=>{
-    const resp= await moviesDB.get<MovieFull>(`/${movieId}`)
+    const movieDetailPromise = await moviesDB.get<MovieFull>(`/${movieId}`)
+    const movieCreditsPromise = await moviesDB.get<CreditsMovie>(`/${movieId}/credits`)
+ 
 
-console.log(resp.data.overview)
+    const[movieDetailResp, movieCreditResp]= await Promise.all([movieDetailPromise, movieCreditsPromise])
+
+setState({
+    isLoading:false,
+    movieFull:movieDetailResp.data,
+    cast:movieCreditResp.data.cast,
+})
+
 }
 
 useEffect(()=>{
@@ -25,7 +41,7 @@ useEffect(()=>{
 }, []);
 
 return {
-    state
+    ...state
 }
 }
 
